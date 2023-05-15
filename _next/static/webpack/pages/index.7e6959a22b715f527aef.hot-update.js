6d7355fb8ff0a3ef0433afdf16bbd44af2ba5dce
AngelVil92 <angelvil92@gmail.com>
2023-05-15 20:28:54 +0200

    actualizada la página de 'el hackathon de hl'

---
+++ _next/static/webpack/pages/index.7e6959a22b715f527aef.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate_N_E("pages/index",{
+
+/***/ "./src/assets/accesibilidad.png":
+/*!**************************************!*\
+  !*** ./src/assets/accesibilidad.png ***!
+  \**************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = \"/_next/static/images/accesibilidad-d6945e0fd710ede1b3f6ed66f5bca790.png\";//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vc3JjL2Fzc2V0cy9hY2Nlc2liaWxpZGFkLnBuZz82NzU2Il0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBIiwiZmlsZSI6Ii4vc3JjL2Fzc2V0cy9hY2Nlc2liaWxpZGFkLnBuZy5qcyIsInNvdXJjZXNDb250ZW50IjpbIm1vZHVsZS5leHBvcnRzID0gXCIvX25leHQvc3RhdGljL2ltYWdlcy9hY2Nlc2liaWxpZGFkLWQ2OTQ1ZTBmZDcxMGVkZTFiM2Y2ZWQ2NmY1YmNhNzkwLnBuZ1wiOyJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/assets/accesibilidad.png\n");
+
+/***/ })
+
+})
