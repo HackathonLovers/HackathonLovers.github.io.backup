c1c627abbc95a9d4468448b3b4eb6f2e6e3d6708
AngelVil92 <angelvil92@gmail.com>
2023-07-12 22:19:03 +0200

    add murena and update agenda

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4a9f3040257776ee0343";
+/******/ 	var hotCurrentHash = "ac68b29c76bbcbc5db92";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
